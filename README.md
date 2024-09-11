78e4e6a6a4c2420acba7eb2864c99aebf3454d88
Mikołaj Neubauer <nicholas.neubauer.developer@gmail.com>
2024-09-11 22:03:56 +0200

    Added the script itself and edited README file.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # hyprland-activity
 Python script to monitor and display app usage for hyprland
+
+Still in testing phase
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='hyprland-apptime',
+    version='1.0.0',
+    author_email='mikolaj_archusr@tutamail.com',
+    description='Python script to monitor and display app usage for hyprland.',
+    url='https://github.com/tpaau-17DB/hyprland-activity.git',
+    packages=['hyprland-activity'],
+    install_requires=["logger"]
+)
